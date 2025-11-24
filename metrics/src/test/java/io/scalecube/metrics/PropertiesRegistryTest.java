0386b76335d886aa66742d73a5adafde3c014017
Artem Vysochyn <artem-v@users.noreply.github.com>
2025-11-24 11:59:09 +0200

    Renamings (#9)

--- metrics/src/test/java/io/scalecube/metrics/PropertiesRegistryTest.java
+++ metrics/src/test/java/io/scalecube/metrics/PropertiesRegistryTest.java
@@ -33,7 +33,7 @@ class PropertiesRegistryTest {
     final var value = UUID.randomUUID();
     final var name = "string_property";
     propertiesRegistry.put(name, value);
-    assertEquals(value.toString(), PropertiesRegistry.getString(countersManager, name));
+    assertEquals(value.toString(), PropertiesRegistry.getStringProperty(countersManager, name));
   }
 
   @Test
@@ -41,7 +41,7 @@ class PropertiesRegistryTest {
     final String name = "int_property";
     int value = 123456789;
     propertiesRegistry.put(name, value);
-    assertEquals(Integer.valueOf(value), PropertiesRegistry.getInt(countersManager, name));
+    assertEquals(Integer.valueOf(value), PropertiesRegistry.getIntProperty(countersManager, name));
   }
 
   @Test
@@ -49,7 +49,7 @@ class PropertiesRegistryTest {
     final String name = "long_property";
     long value = 9876543210L;
     propertiesRegistry.put(name, value);
-    assertEquals(Long.valueOf(value), PropertiesRegistry.getLong(countersManager, name));
+    assertEquals(Long.valueOf(value), PropertiesRegistry.getLongProperty(countersManager, name));
   }
 
   @Test
@@ -57,19 +57,21 @@ class PropertiesRegistryTest {
     final String name = "enum_property";
     SampleEnum value = SampleEnum.B;
     propertiesRegistry.put(name, value);
-    SampleEnum result = PropertiesRegistry.getEnum(countersManager, name, SampleEnum::valueOf);
+    SampleEnum result =
+        PropertiesRegistry.getEnumProperty(countersManager, name, SampleEnum::valueOf);
     assertEquals(value, result);
   }
 
   @Test
   void unknownPropertyReturnsNull() {
-    assertNull(PropertiesRegistry.getString(countersManager, "nonexistent"));
-    assertNull(PropertiesRegistry.getInt(countersManager, "nonexistent"));
-    assertNull(PropertiesRegistry.getInt(countersManager, "nonexistent"));
-    assertNull(PropertiesRegistry.getInt(countersManager, "nonexistent"));
-    assertNull(PropertiesRegistry.getLong(countersManager, "nonexistent"));
-    assertNull(PropertiesRegistry.getBoolean(countersManager, "nonexistent"));
-    assertNull(PropertiesRegistry.getEnum(countersManager, "nonexistent", SampleEnum::valueOf));
+    assertNull(PropertiesRegistry.getStringProperty(countersManager, "nonexistent"));
+    assertNull(PropertiesRegistry.getIntProperty(countersManager, "nonexistent"));
+    assertNull(PropertiesRegistry.getIntProperty(countersManager, "nonexistent"));
+    assertNull(PropertiesRegistry.getIntProperty(countersManager, "nonexistent"));
+    assertNull(PropertiesRegistry.getLongProperty(countersManager, "nonexistent"));
+    assertNull(PropertiesRegistry.getBooleanProperty(countersManager, "nonexistent"));
+    assertNull(
+        PropertiesRegistry.getEnumProperty(countersManager, "nonexistent", SampleEnum::valueOf));
   }
 
   enum SampleEnum {
